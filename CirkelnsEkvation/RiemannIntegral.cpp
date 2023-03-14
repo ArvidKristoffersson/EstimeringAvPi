2b354f49b063600b06d8f8d8510be98d5a5f1054
ArvidKristoffersson <117284329+ArvidKristoffersson@users.noreply.github.com>
2023-03-14 16:43:21 +0100

    Create RiemannIntegral.cpp

---
+++ CirkelnsEkvation/RiemannIntegral.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <cmath>
+
+#pragma GCC optimize("Ofast,inline") 
+#pragma GCC optimize ("unroll-loops")
+
+using namespace std;
+
+inline void scoobydoobydoo(){
+    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+double f(double x){
+    return pow((1 - pow(x,2)), 0.5);
+}
+
+int main(){
+    scoobydoobydoo();
+    double n; cin >> n;
+    int from, to; cin >> from >> to;
+
+    cout << fixed << setprecision(20);
+
+    double jump = (double)(to-from)/n;
+
+    double sum = 0;
+
+    for (double i = from + jump/2.0; i <= to; i += jump){
+        sum += f(i)*jump;
+    }
+
+    cout << sum * 2 << endl;
+
+
+    return 0;
+}
