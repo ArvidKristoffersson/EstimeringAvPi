7d5ad6f652577936c18aee29d9016edc31d94c88
ArvidKristoffersson <117284329+ArvidKristoffersson@users.noreply.github.com>
2023-03-14 21:51:58 +0100

    Create chudnovsky.py

---
+++ Omskrivningar/chudnovsky.py
@@ -0,0 +1,34 @@
+from decimal import *
+import math
+dp = [1]
+for i in range(0, 1001):
+    dp.append(0)
+
+getcontext().prec = 100
+
+def factorial(x):
+    if x == 0:
+        return 1
+    if dp[x] < 1:
+        dp[x] = factorial(x-1)*x
+    return dp[x]
+
+
+factorial(100)
+    
+
+def f(q):
+    return Decimal(((-1)**q * dp[6*q] * (545140134*q+13591409))/(dp[3*q]*(dp[q]**3)*((640320)**(3*q+1.5))))
+
+inp = int(input())
+
+sum = 0
+
+
+for i in range(0, inp):
+    sum += 12 * f(i)
+    print(i, Decimal(1)/Decimal(sum))
+
+
+print()
+print(Decimal(1)/Decimal(sum))
