3ac158f095d220eb64b66f8953f11741c4b8740a
karlsoderlund <128074695+karlsoderlund@users.noreply.github.com>
2023-03-17 12:56:21 +0100

    Update arctanStegMetod.py

--- SmåttOchGott/arctanStegMetod.py
+++ SmåttOchGott/arctanStegMetod.py
@@ -1,8 +1,9 @@
 #Max input för exekvering under 1 sekund är ungefär 10 000 000
 #Då får den rätt på typ 5 siffror
+#nvm koden var fel, den klarar typ 12 siffror
 
 a = int(input())
 sist=0
 for i in range(a):
     sist += 4*a/(a**2+i**2)
-print(sist)
+print(sist-1/a)
